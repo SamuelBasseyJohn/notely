f4d71e82dcca13cd629aafe5b42024e616b97838
SamuelBasseyJohn <88030818+SamuelBasseyJohn@users.noreply.github.com>
2022-06-08 06:51:28 +0100

    Deleted Unnecessary File

--- lib/Widgets/colorpallets.dart
+++
@@ -1,4 +0,0 @@
-// orange : "FA5B3D"
-// grey (i guess): "37474F"
-// white : "FFFFFF"
-// lighter grey: "d6d6d6"
--- lib/Widgets/navigating.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter/material.dart';
-// create a navigation path that makes life less stressful for you biko
